1953539606e99716682978e941dcbf203eae4016
David L Cervantes <davidlcervantes@Davids-MacBook-Pro.local>
2018-09-30 21:20:58 -0700

    php practice with variables

--- index.php
+++ index.php
@@ -1,7 +1,18 @@
 <?php get_header(); ?>
 
 <div>
-  <h1>Working No Instructor!!</h1>
+  <h1>
+    PHP Practice
+  </h1>
 </div>
+
+<?php 
+  $number1 = 20;
+  $number2 = 35;
+
+  $answer = $number2 - $number1;
+
+  echo $answer
+?>
 
 <?php get_footer(); ?>
